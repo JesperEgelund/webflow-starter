c5ff2142f19eab880b48258e923f7d916fee3c25
Jesper Egelund <JesperEgelund@users.noreply.github.com>
2023-10-12 11:01:36 +0200

    added color-mode script

--- index.js
+++ index.js
@@ -5,4 +5,22 @@ $(document).ready(function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button and page wrapper elements
+    var colorModeBtn = document.getElementById('colorModeBtn');
+    var body = document.querySelector('body');
+
+    // Add a click event listener to the button
+    colorModeBtn.addEventListener('click', function () {
+      // Get the current value of the "color-mode" attribute
+      var currentMode = body.getAttribute('color-mode');
+
+      // Toggle between "light" and "dark"
+      var newMode = currentMode === 'dark' ? 'light' : 'dark';
+
+      // Update the "color-mode" attribute with the new value
+      body.setAttribute('color-mode', newMode);
+    });
+  });
+
 console.log("Hello there!");
