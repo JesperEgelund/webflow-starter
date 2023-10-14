b0f122aa1f257749a2fe01ed2018f341d8f564b6
Jesper Egelund <JesperEgelund@users.noreply.github.com>
2023-10-14 10:47:04 +0200

    update

--- index.js
+++ index.js
@@ -4,23 +4,3 @@ $(document).ready(function() {
         $('.toggle_component').toggleClass('is-on');
     });
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the button and page wrapper elements
-    var colorModeBtn = document.getElementById('colorModeBtn');
-    var body = document.querySelector('body');
-
-    // Add a click event listener to the button
-    colorModeBtn.addEventListener('click', function () {
-      // Get the current value of the "color-mode" attribute
-      var currentMode = body.getAttribute('color-mode');
-
-      // Toggle between "light" and "dark"
-      var newMode = currentMode === 'dark' ? 'light' : 'dark';
-
-      // Update the "color-mode" attribute with the new value
-      body.setAttribute('color-mode', newMode);
-    });
-  });
-
-console.log("Hello there!");
